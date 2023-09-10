9355f725d2ea0d62c0d11c522d3069ded13c0bed
Ethical-Jase <122585306+Ethical-Jase@users.noreply.github.com>
2023-09-10 11:13:45 +1000

    Comments added for each command

--- filter_logs_pandas.py
+++ filter_logs_pandas.py
@@ -18,37 +18,56 @@ for file in files_list:
 # concat the files into a single dataframe
 awsc = pd.concat(awsc_list).reset_index(drop=True)
 
+# display top 5 rows
 awsc.head()
 
+# displays specific columns
 awsc[['eventTime', 'awsRegion', 'eventName', 'userIdentity_userName']]
 
+# allows mixed formats to be used
 awsc['eventTime'] = pd.to_datetime(awsc['eventTime'],dayfirst=True, format='mixed')
 
+#displays types e.g objects
+awsc.dtypes
+
+# sets time and date to current
 from datetime import datetime
 now  = datetime.now()
 
+# sorts date range greater than and less than
 dateMask = (awsc['eventTime'] > '2014-03-06 17:00:00') & (awsc['eventTime'] < '2014-03-06 20:00:00')
 df=awsc.loc[dateMask]
 df.head(10)
 
+# sorts for specific ip address and display top 10 rows 
 ipmask = (awsc['sourceIPAddress']=='205.251.233.176') 
 dfip=awsc.loc[ipmask]
 dfip.head(10)
 
+# will display rows 1 and 2
 awsc.iloc[1:3]
 
+# sorts assending
 awsc.sort_values('eventTime', ascending = True)
 
+# sorts decending
 awsc.sort_values('eventTime', ascending = False)
 
+# looks for specific ip address
 awsc['sourceIPAddress'] == ('72.21.198.64')
 
+# looks for srting containing "api"
 awsc['userAgent'].str.contains('api')
 
+# displays all ip address and a count of each 
 awsc.groupby('sourceIPAddress').count()
 
+# displays eventName and eventTime columns and a count of each
 awsc.groupby(['eventName', 'eventTime']).count()
 
+# will return an interger of total number of different eventTimes found
 awsc['eventTime'].nunique()
 
+# will return each a list of each different result found in eventTime column
 awsc['eventTime'].unique()
+
